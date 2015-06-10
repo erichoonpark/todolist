1af520776b69fb8ae12ecc8209e3becd6a56e858
erichoonpark <erichoonpark@gmail.com>
2015-06-09 20:21:26 -0700

    Test Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,8 @@
 class UsersController < ApplicationController
   def show
-    if params[:id].present?
-      @user = User.find(params[:id])
-      @item = current_user.items
-    else
-      @user = current_user
-      @item = current_user.items
-    end
+      @user = current_user ? current_user : User.find(params[:id])
+      @items = @user.items
+      @item = @user.items.new
   end
 
 
